a5bf3a2c8cfde2d393f6780334f68ace44d00db0
karen1au <kagemikaka@hotmail.com>
2019-03-12 17:24:28 +0000

    added loop to create list of urls

--- express_server.js
+++ express_server.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, test:"<h1>Karen</h1>" };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
