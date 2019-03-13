19eeaf3723064eac0bf71d8c3e90e9fb384add4d
karen1au <kagemikaka@hotmail.com>
2019-03-13 13:06:23 +0000

    added edit long url feature

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,23 +14,23 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+//page for all urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+//create form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
+//create short url
 app.post("/urls", (req, res) => {
   console.log(req.body);
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
-
+//redirect to real site
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   if (!longURL){
@@ -38,11 +38,9 @@ app.get("/u/:shortURL", (req, res) => {
   } else {
     res.redirect(longURL);
   }
-
   console.log("status code: ",res.statusCode);
 });
-
-
+//to the indivodual short url page
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = {
@@ -51,13 +49,18 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-
+//delete url
 app.post("/urls/:shortURL/delete", (req, res) =>{
   let shortURL = req.params.shortURL;
   console.log("deleted");
   delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
+//update url
+app.post("/urls/:shortURL/edit", (req, res) =>{
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls');
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
